865e2bf4cc8c6fac7ada6928096c0276732532d1
TFMahabub <rjmahabub543@gmail.com>
2022-11-08 22:15:53 +0600

    why choose me section add

--- src/components/Pages/Home/ContactUs/Contact.js
+++ src/components/Pages/Home/ContactUs/Contact.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import Address from './Address';
-import HotLine from './HotLine';
-import Time from './Time';
 
 const Contact = () => {
   return (
-    <div className='mt-16  rounded-lg'>
-      <div className="bg-green md:my-16 lg:py-16 py-10 w-full md:mx-24 md:px-12 px-4 flex flex-col items-center justify-center relative z-40">
-        <h1 className="text-4xl font-semibold leading-9 text-white text-center">Don’t miss out!</h1>
-          <p className="text-base leading-normal text-center text-white mt-6">
-            Subscribe to your newsletter to stay in the loop. Our newsletter is sent once in <br />a week on every friday so subscribe to get latest news and updates.
-          </p>
-          <div className="sm:border border-white flex-col sm:flex-row  flex items-center lg:w-5/12 w-full mt-12 space-y-4 sm:space-y-0">
-            <input className="border border-white sm:border-transparent text-base w-full font-medium leading-none text-white p-4 focus:outline-none" placeholder="Email Address" />
-            <button className="focus:outline-none focus:ring-offset-2 focus:ring border border-white sm:border-transparent w-full sm:w-auto bg-white py-4 px-6 hover:bg-opacity-75">Subscribe</button>
-          </div>
+    <section className='grid grid-cols-1 md:grid-cols-2 mt-32'>
+      <div>
+        <img className='w-full' src="https://i.ibb.co/kVng4Vc/tour-guide.jpg" alt="" />
       </div>
-    </div>
+      <div className='bg-light-gray p-16'>
+        <h2 className='text-5xl font-bold text-[#fff] tracking-wider line'>Why Choose Me?</h2>
+        <p className='text-lg mt-4 text-[#cccccc]'>There're many option you can see but I give you the best services and discount. Also you'll get more and more benefits explore my exiting services in this site</p>
+        <div className='mt-12'>
+          <h3 className='text-2xl font-semibold text-[#fff] tracking-wide'>Professional and Certified</h3>
+          <p className='text-lg text-[#cccccc]'>I'm a govment certifird tour guide. I also get some awords for my greatest services. Don't miss the offer</p>
+        </div>
+        <div className='mt-12'>
+          <h3 className='text-2xl font-semibold text-[#fff] tracking-wide'>Get Instant Tour Bookings</h3>
+          <p className='text-lg text-[#cccccc]'>It's very easy to book my services just folow the direction of this website. It's very simple and easy</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
